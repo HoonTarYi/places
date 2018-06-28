0aae10b9b212d803dd39ee9d5c378c0c0c889c2c
student <kaizukahayate@gmail.com>
2018-06-28 18:17:22 +0800

    V2

--- filemgr.js
+++ filemgr.js
@@ -2,9 +2,9 @@ const {MongoClient} = require('mongodb');
 const fs = MongoClient;
 
 // const database = 'mongodb://localhost:27017';
-const database ='mongodb://placesapp97:hayate972:D@ds121301.mlab.com:21301/placesapp97';
+const database ='mongodb://places123:places123@ds121301.mlab.com:21301/placesapp97';
 
-const appname ='places';
+const appname ='placesapp97';
 const collectionname ='placesappcollection';
 const saveData = (newdata) =>  {
   return new Promise((resolve,reject) => {
